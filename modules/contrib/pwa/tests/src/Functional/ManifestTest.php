4d683259c156b9bf819f8227464e34961e808f9f
Alexis <alexis.saransig@kodffe.com>
2023-08-14 13:05:33 -0500

    Updating directory

---
+++ modules/contrib/pwa/tests/src/Functional/ManifestTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\Tests\pwa\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test description.
+ *
+ * @group pwa
+ */
+class ManifestTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['pwa'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Test caching of manifest.json.
+   */
+  public function testCache() {
+    // Create a test user.
+    $user = $this->drupalCreateUser([
+      'access content',
+    ]);
+    $this->drupalLogin($user);
+    $this->drupalGet('manifest.json');
+    $assert = $this->assertSession();
+    $assert->statusCodeEquals(200);
+    $assert->responseHeaderContains('X-Drupal-Cache-Tags', 'manifestjson');
+  }
+
+}
