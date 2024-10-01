2800999e44133b0e93bbce52502b70398881693f
Alexis <alexis.saransig@kodffe.com>
2024-10-01 13:22:26 -0500

    Updating to web dir.

--- themes/contrib/ignite_theme/.storybook/manager.js
+++
@@ -1,7 +0,0 @@
-// Defines custom Storybook theme.
-import { addons } from '@storybook/addons';
-import IgniteTheme from './ignite-theme';
-
-addons.setConfig({
-  theme: IgniteTheme,
-});
--- themes/custom/kodffe_theme/.storybook/manager.js
+++
@@ -1,7 +0,0 @@
-// Defines custom Storybook theme.
-import { addons } from '@storybook/addons';
-import Kodffe_Theme from './kodffe-theme';
-
-addons.setConfig({
-  theme: Kodffe_Theme,
-});
